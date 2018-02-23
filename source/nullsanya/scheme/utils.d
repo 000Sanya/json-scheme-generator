584d0988d8e3be25fa538c8bb01e69b4a8843070
000Sanya <000sanya.000sanya@gmail.com>
2018-02-23 17:49:31 +1000

    fixes for utils.d

--- source/nullsanya/scheme/utils.d
+++ source/nullsanya/scheme/utils.d
@@ -1,24 +1,34 @@
 module nullsanya.scheme.utils;
 
 import std.json;
+import std.traits : isArray, isAssociativeArray, isSigned, isUnsigned, isFloatingPoint;
+
+template ArrayElementType(T : T[])
+{
+    alias ArrayElementType = T;
+}
+
+template AATypes(T : Value[Key], Value, Key)
+{
+    alias key = Key;
+    alias value = Value;
+}
 
 bool hasType(T)(ref inout(JSONValue) value)
 {
-    import std.traits : isArray, isAssociativeArray;
-
     static if(is(T == bool))
     {
         return (value.type == JSON_TYPE.TRUE || value.type == JSON_TYPE.FALSE);
     }
-    else static if(is(T == double) || is(T == float))
+    else static if(is(T : real) && isFloatingPoint!T)
     {
         return value.type == JSON_TYPE.FLOAT;
     }
-    else static if(is(T == ulong) || is(T == uint))
+    else static if(is(T : ulong) && isUnsigned!T)
     {
         return value.type == JSON_TYPE.UINTEGER;
     }
-    else static if(is(T == long) || is(T == int))
+    else static if(is(T : long) && isSigned!T)
     {
         return value.type == JSON_TYPE.INTEGER;
     }
@@ -32,7 +42,39 @@ bool hasType(T)(ref inout(JSONValue) value)
     }
     else static if(isAssociativeArray!T)
     {
-        // TODO: implement
+        static if(is(AATypes!(T).key : string))
+        {
+            return value.type == JSON_TYPE.OBJECT;
+        }
+        else static assert(0, "Not supported type");
     }
     else static assert(0, "Not supported type");
+}
+
+unittest
+{
+    string json = q{
+        {
+            "a" : 3,
+            "b" : 2.4,
+            "c" : true,
+            "d" : false,
+            "e" : -3,
+            "f" : "hello",
+            "g" : ["1", 2, true],
+            "h" : {
+                "_t" : 1
+            }
+        }
+    };
+    JSONValue values = parseJSON(json);
+
+    //assert(hasType!uint(values["a"]));
+    assert(hasType!double(values["b"]));
+    assert(hasType!bool(values["c"]));
+    assert(hasType!bool(values["d"]));
+    assert(hasType!int(values["e"]));
+    assert(hasType!string(values["f"]));
+    assert(hasType!(JSONValue[])(values["g"]));
+    assert(hasType!(JSONValue[string])(values["h"]));
 }
